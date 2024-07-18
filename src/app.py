ab23088820034f6dab3b6549bbbf0b020a606862
srinman <smanivel@microsoft.com>
2024-07-18 08:41:19 -0400

    simple hello world without args

--- src/app.py
+++ src/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get('name')
-    return 'Hello, ' + name + '!'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
